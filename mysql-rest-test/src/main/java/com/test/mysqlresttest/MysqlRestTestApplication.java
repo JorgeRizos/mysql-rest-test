180e384abdcc2100b085876d7578e2e4bbd0afbc
JorgeRizos <jortageta@gmail.com>
2020-10-30 12:50:57 +0100

    MRT00 - Initial commit
    Initial comit with only the project base and an improved gitignore

---
+++ mysql-rest-test/src/main/java/com/test/mysqlresttest/MysqlRestTestApplication.java
@@ -0,0 +1,13 @@
+package com.test.mysqlresttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlRestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlRestTestApplication.class, args);
+	}
+
+}
---
+++ mysql-rest-test/src/test/java/com/test/mysqlresttest/MysqlRestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.mysqlresttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlRestTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
